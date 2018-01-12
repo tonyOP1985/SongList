53f4597131b94e9a3e6a41846fdae58b5f041b1e
tonyOP1985 <toliverpaull@gmail.com>
2018-01-12 18:02:30 -0500

    add songs Schema

---
+++ models/Songs.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// create Schema
+const SongSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  artist: {
+    type: String,
+    required: true
+  },
+  songKey: {
+    type: String,
+    require: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+mongoose.model('songs', SongSchema)
