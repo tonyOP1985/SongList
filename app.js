9cc8c6bb1d55c33014072c66ff27935952768f68
tonyOP1985 <toliverpaull@gmail.com>
2018-01-13 14:09:33 -0500

    Added put request to edit songs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-
+const methodOverride = require('method-override')
 
 const app = express()
 
@@ -27,6 +27,9 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// Method Overrider middleware
+app.use(methodOverride('_method'))
+
 //index route
 app.get('/', (req, res) => {
   const title = 'Welcome'
@@ -39,9 +42,32 @@ app.get('/about', (req, res) => {
   res.render('about')
 })
 
+// song index page
+app.get('/songs', (req, res) => {
+  Song.find({})
+    .sort({date: 'desc'})
+    .then(songs => {
+      res.render('songs/index', {
+        songs: songs
+      })
+    })
+})
+
 // Add song form
 app.get('/songs/add', (req, res) => {
   res.render('songs/add')
+})
+
+// Edit Song form
+app.get('/songs/edit/:id', (req, res) => {
+  Song.findOne({
+    _id: req.params.id
+  })
+  .then(song => {
+    res.render('songs/edit', {
+      song: song
+    })
+  })
 })
 
 // Process Form
@@ -50,19 +76,46 @@ app.post('/songs', (req, res) => {
 
   if (!req.body.title) errors.push({text: 'Please add a title'})
   if (!req.body.artist) errors.push({text: 'Please add an artist'})
-  if (!req.body.key) errors.push({text: 'Please add a key'})
+  if (!req.body.songKey) errors.push({text: 'Please add a key'})
 
   if (errors.length) {
     res.render('songs/add', {
       errors: errors,
       title: req.body.title,
       artist: req.body.artist,
-      key: req.body.key
+      key: req.body.songKey
     })
   } 
   else {
-    res.send('passed')
+    const newUser = {
+      title: req.body.title,
+      artist: req.body.artist,
+      songKey: req.body.songKey
+    }
+    new Song(newUser)
+      .save()
+      .then(song => {
+        res.redirect('/songs')
+      })
   }
+})
+
+// Edit form process
+app.put('/songs/:id', (req, res) => {
+  Song.findOne({
+    _id: req.params.id
+  })
+  .then(song => {
+    //new values
+    song.title = req.body.title
+    song.artist = req.body.artist
+    song.songKey = req.body.songKey
+
+    song.save()
+      .then(song => {
+        res.redirect('/songs')
+      })
+  })
 })
 
 const port = 5000
